73a12a8a84be613eb562ffd2943beaf3d90ca1c8
proxim <61657192+proxim@users.noreply.github.com>
2022-09-20 19:49:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+SECRET = os.getenv('SECRET')
+SECRET2 = os.getenv('ANOTHER_SECRET')
+
+
 def is_even(num):
     return num % 2 == 0
 
@@ -5,4 +13,4 @@ def is_odd(num):
     return not is_even(num)
 
 
-print(is_even(2))
+print(SECRET2)
